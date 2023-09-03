fdc5cbe6ebd4eed9fc010d5bdbafc222906fbcd1
K Srinivas Rao <ksrinivasrao531@gmail.com>
2023-09-03 08:56:06 +0530

    initializing

---
+++ packages/react/tsup.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from "tsup";
+
+import { config } from "@sse-kit/tsup-config";
+
+export default defineConfig((opts) => ({
+  ...config,
+  entry: ["./src/index.ts", "./src/hooks.ts"],
+  clean: !opts.watch,
+  esbuildOptions: (option) => {
+    option.banner = {
+      js: `"use client";`,
+    };
+  },
+}));
